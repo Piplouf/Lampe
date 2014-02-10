199bb53e81f3b24223c4caaeb1ab274cbb4cd4ea
maruejof <maruejof@PC-B102-06.iut-valence.fr>
2014-02-10 10:32:26 +0100

    Initial commit

---
+++ src/Lampe.java
@@ -0,0 +1,76 @@
+
+/**
+ * Classe lampe
+ * @author maruejof
+ *
+ */
+public class Lampe {
+	
+	
+	/**
+	 * Puissance de la lampe
+	 */
+	private int puissance;
+	/**
+	 * Booléen pour savoir si la lampe est allumée
+	 */
+	private boolean estAllumee;
+	
+	/**
+	 * Puissance par défaut
+	 */
+	private final static int PUISSANCE_DEFAUT = 50;
+
+	
+	/**
+	 * Constructeur de base
+	 * Puissance = 50
+	 * estAllumee = true
+	 */
+	public Lampe(){
+		this(Lampe.PUISSANCE_DEFAUT);
+	}
+	
+	/**
+	 * Constructeur demandant la puissance
+	 * @param puissance
+	 */
+	public Lampe(int puissance){
+		
+		this.puissance = puissance;
+		this.estAllumee = true;
+	}
+	
+	/**
+	 * 
+	 * @return puissance
+	 */
+	public int getPuissance() {
+		return this.puissance;
+	}
+	
+	
+	/**
+	 * 
+	 * @return estAllumee
+	 */
+	public boolean isEstAllumee() {
+		return this.estAllumee;
+	}
+	
+	/**
+	 * Méthode permettant d'allumer une lampe
+	 */
+	public void allumer(){
+		this.estAllumee= true;
+	}
+	
+	/**
+	 * Méthode permettant d'étinedre une lampe
+	 */
+	public void eteindre(){
+		this.estAllumee= false;
+	}
+	
+	
+}
---
+++ src/Main.java
@@ -0,0 +1,25 @@
+
+/**
+ * Programme principal
+ * @author maruejof
+ *
+ */
+public class Main {
+
+	/**
+	 * Méthode principale.
+	 *
+	 * @param args the arguments
+	 */
+	public static void main(String[] args) {
+		
+		Lampe maLampe = new Lampe(500);
+		
+		maLampe.eteindre();
+		System.out.println(maLampe.isEstAllumee());
+		System.out.println(maLampe.getPuissance());
+		
+
+	}
+
+}
